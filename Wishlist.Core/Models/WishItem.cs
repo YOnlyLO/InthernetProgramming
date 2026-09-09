8e1eac321bdd8f05e57a252274be91e999297b46
Danil Stepanov <danilstep04@mail.ru>
2026-09-09 10:06:56 +0300

    Добавлена базовая слоенная архитектура приложения. Добавлена первоначальная модель отслеживаемого подарка (WishItem)

---
+++ Wishlist.Core/Models/WishItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Wishlist.Core;
+
+public class WishItem
+{
+    private string Name { get; } = string.Empty;
+    private string Description { get; } = string.Empty;
+    private decimal Cost { get; }
+    private string ItemUrl { get; } = string.Empty;
+    public WishItem(string name, string description, decimal cost, string itemUrl)
+    {
+        Name = name;
+        Description = description;
+        Cost = cost;
+        ItemUrl = itemUrl;
+    }
+}
